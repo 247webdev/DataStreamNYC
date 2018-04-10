8a336b4c28af62bcade25061e7ac56ea0f19376a
Christian Haberman <slimdoja@outlook.com>
2018-04-10 11:32:07 -0700

    Added user model

---
+++ src/main/java/com/example/DataStreamNYC/models/User.java
@@ -0,0 +1,34 @@
+package com.example.DataStreamNYC.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor @Getter @Setter
+@Entity @Table(name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    public User(String email, String firstName, String lastName, String password) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+}
