c01b67fe226813a0d8a2dc926cf31ab3f2bb449e
Christian Haberman <slimdoja@outlook.com>
2018-04-10 13:19:43 -0700

    Added Users Controller and Unit testing

---
+++ users-api/src/test/java/com/example/usersapi/controllers/UsersControllerTests.java
@@ -0,0 +1,27 @@
+package com.example.usersapi.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UsersController.class)
+public class UsersControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void findAllUsers_success_returnsStatusOK() throws Exception {
+
+        this.mockMvc
+                .perform(get("/"))
+                .andExpect(status().isOk());
+    }
+}
