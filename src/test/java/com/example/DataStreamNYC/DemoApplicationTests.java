291f1b70c5851ac1c92b46ec0f7235dc2a5c0b3f
Christian Haberman <slimdoja@outlook.com>
2018-04-10 11:48:52 -0700

    Added feature tests for users-api

--- src/test/java/com/example/DataStreamNYC/DemoApplicationTests.java
+++ src/test/java/com/example/DataStreamNYC/DemoApplicationTests.java
@@ -1,29 +1,16 @@
 package com.example.DataStreamNYC;
 
-import com.example.DataStreamNYC.repositories.UserRepository;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest
 public class DemoApplicationTests {
 
-	@Autowired
-	private UserRepository userRepository;
-
-	@Before
-	public void setUp() {
-		userRepository.deleteAll();
-	}
-
-	@After
-	public void tearDown() {
-		userRepository.deleteAll();
+	@Test
+	public void contextLoads() {
 	}
 
 }
