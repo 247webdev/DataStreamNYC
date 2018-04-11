3143818d64de78e4b38535370449276c9c4c7928
Christian Haberman <slimdoja@outlook.com>
2018-04-11 14:22:04 -0700

    Added base for UI testing for users

--- src/test/java/com/example/DataStreamNYC/features/UsersUIFeatureTests.java
+++ src/test/java/com/example/DataStreamNYC/features/UsersUIFeatureTests.java
@@ -6,11 +6,17 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.stream.Stream;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -38,6 +44,8 @@ public class UsersUIFeatureTests {
                 "last1",
                 "password1"
         );
+        firstUser = userRepository.save(firstUser);
+        Long firstUserId = firstUser.getId();
 
         User secondUser = new User(
                 "user2@email.com",
@@ -45,11 +53,21 @@ public class UsersUIFeatureTests {
                 "last2",
                 "password2"
         );
+        secondUser = userRepository.save(secondUser);
+        Long secondUserId = secondUser.getId();
 
-        Stream.of(firstUser, secondUser)
-                .forEach(user -> {
-                    userRepository.save(user);
-                });
+        System.setProperty("selenide.browser", "Chrome");
 
+        // Visit the UI in a browser
+        open("http://localhost:3000/");
+
+        // Visit the admin view page
+        $("#admin-view-link").click();
+
+        // Make sure the link worked and the form is now showing
+        $("#users-wrapper").should(appear);
+
+        // There should only be two users
+        $$("[data-user-display]").shouldHave(size(2));
     }
 }
