46a9a8d423bda4d348d1708e2bc00331603a66d9
Christian Haberman <slimdoja@outlook.com>
2018-04-11 15:56:08 -0700

    fixed styling for homepage

--- front-end/src/components/HomePage.js
+++ front-end/src/components/HomePage.js
@@ -5,21 +5,27 @@ import '../App.css';
 const HomePage = (props) => {
   return (
     <div className="home">
-      <div className="NYClogo">
-        <div className="logoleft">
-          <h1>NYC</h1>
-          <div className="logoright">
-            <p>Citywide</p>
-            <p>Administrative</p>
-            <p>Services</p>
+      <div className="homemid">
+        <div className="hometop">
+          <div className="NYClogo">
+            <div className="logoleft">
+              <h1>NYC</h1>
+              <div className="logoright">
+                <p>Citywide</p>
+                <p>Administrative</p>
+                <p>Services</p>
+              </div>
+            </div>
+            <h2>The City Record Online</h2>
+          </div>
+          <div className="homenav">
+            <Link to="/apidashboard">API Dashboard</Link> |&nbsp;
+            <Link to="/suggestionboard">Suggestion Board</Link> |&nbsp;
+            <Link to="/admin" id="admin-view-link">Admin View</Link>
           </div>
         </div>
-        <h2>The City Record Online</h2>
+        <h3>Welcome!</h3>
       </div>
-      <h3>Welcome!</h3>
-      <Link to="/apidashboard">API Dashboard</Link> |&nbsp;
-      <Link to="/suggestionboard">Suggestion Board</Link> |&nbsp;
-      <Link to="/admin" id="admin-view-link">Admin View</Link>
       <div className="skyline"></div>
     </div>
   );
