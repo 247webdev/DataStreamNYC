bdb000a7d496dc372a40488fe7ff48dde9291851
Christian Haberman <slimdoja@outlook.com>
2018-04-11 19:40:41 -0700

    Created QueryForm component

---
+++ front-end/src/components/QueryForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class QueryForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      query: ''
+    };
+  }
+
+  handleChange = (event) => {
+    const newValue = event.target.value.toUpperCase();
+
+    let updatedQuery = { ...this.state.query };
+    updatedQuery = newValue;
+    
+    this.setState({ query: updatedQuery });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.runSearch(this.state.query);
+  };
+
+  render() {
+    return (
+      <div className="forms">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="agency">Agency Name:&nbsp;
+            <input type="text" name="agency" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default QueryForm;
