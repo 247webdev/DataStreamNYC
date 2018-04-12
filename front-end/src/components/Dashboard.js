c20c6c73960c5eaa1ea03f9e978f3530f2cb2670
Christian Haberman <slimdoja@outlook.com>
2018-04-11 19:39:30 -0700

    Created dashboard component

---
+++ front-end/src/components/Dashboard.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import '../App.css';
+
+import QueryForm from './QueryForm';
+import QueryList from './QueryList';
+
+class Dashboard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      results: []
+    };
+  }
+
+
+  runSearch = async (agency) => {
+    try {
+      const searchResponse = await axios.get(`http://data.cityofnewyork.us/resource/buex-bi6w.json?agency_name=${agency}`);
+      this.setState({ results: searchResponse.data });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <div className="dashboard">
+        <div className="nav">
+          <Link to="/" id="home-page-link">Home</Link> |&nbsp;
+          <Link to="/admin" id="admin-view-link">View Users</Link>
+        </div>
+        <h1 className="white"><u>API Dashboard</u></h1>
+        <QueryForm runSearch={this.runSearch} />
+        <QueryList queries={this.state.results} />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
