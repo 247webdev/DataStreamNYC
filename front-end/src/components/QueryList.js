6a92e0e68d45484f70a7e816f77376b35aa586c9
Christian Haberman <slimdoja@outlook.com>
2018-04-11 19:42:03 -0700

    Created QueryList component

---
+++ front-end/src/components/QueryList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../App.css';
+
+import Query from './Query';
+
+const QueryList = (props) => {
+  return (
+    <div id="query-wrapper">
+      <h3 className="white">Results</h3>
+      {
+        props.queries.map((query, index) => {
+          return (
+            <Query
+              query={query}
+              key={index}
+            />
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default QueryList;
