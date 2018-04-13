949bcaa70149c91198adcbc2019d7d379a193a95
Christian Haberman <slimdoja@outlook.com>
2018-04-12 17:08:42 -0700

    Set up axios call to fetch suggestions from database

--- front-end/src/components/SuggestionBoard.js
+++ front-end/src/components/SuggestionBoard.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const SuggestionBoard = (props) => {
-  return (
-    <div>
-      It Worked!
-    </div>
-  );
+import SuggestionForm from './SuggestionForm';
+import SuggestionList from './SuggestionList';
+
+class SuggestionBoard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      suggestions: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const suggestionsResponse = await axios.get(`${process.env.REACT_APP_USERSAPI}/suggestions`);
+      const suggestionsFromDatabase = suggestionsResponse.data;
+      this.setState({ 
+        suggestions: suggestionsResponse.data,
+        suggestionsResponse
+       });
+    } catch (error) {
+      console.log(error);
+    };
+  };
+
+  createSuggestion = async (newSuggestion) => {
+    try {
+      const newSuggestionResponse = await axios.post(`${process.env.REACT_APP_USERSAPI}/users`, newSuggestion);
+      const newSuggestionFromDatabase = newSuggestionResponse.data;
+
+      const updatedSuggestionsList = [...this.state.suggestions];
+      updatedSuggestionsList.push(newSuggestionFromDatabase);
+
+      this.setState({ suggestions: updatedSuggestionsList });
+    } catch (error) {
+      console.log("Error creating new suggestion");
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <SuggestionForm />
+        <SuggestionList />
+      </div>
+    );
+  }
 }
 
 export default SuggestionBoard;
